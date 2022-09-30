3a48acf6356562dde93cba059a5644b0781c7e9b
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2022-09-30 07:52:44 +0700

    Render circle with color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-
+import Map from "./Map";
 import './style.css';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>My React App!</h1>
+                <Map />
             </div>
         );
     }
---
+++ src/Map.js
@@ -0,0 +1,106 @@
+import mapboxgl from "mapbox-gl";
+import React, { useEffect, useState, useRef } from 'react';
+import "./Map.css";
+import data from "../hochiminh_population_geo.json";
+
+mapboxgl.accessToken = "pk.eyJ1IjoiaG9hbmdxdW9jaHVuZzExMTAiLCJhIjoiY2w4Ym55OXRqMDB0bjNvcGRycDN2MGRuZSJ9.EEFhms7cuNTpkRH-TxfVPw"
+
+const Map = () => {
+    const options = [
+        {
+            name: "Population",
+            description: "Estimated total population in 2019",
+            property: "population_in_2019",
+            colorStops: [
+                [0, '#f8d5cc'],
+                [100000, '#f4bfb6'],
+                [200000, '#f1a8a5'],
+                [300000, '#ee8f9a'],
+                [400000, '#ec739b'],
+                [500000, '#dd5ca8'],
+                [600000, '#c44cc0'],
+                [700000, '#9f43d7'],
+                [800000, '#6e40e6']
+            ],
+            radiusStops: [
+                [100, 0.01],
+                [1000, 1],
+                [2000, 2],
+                [3000, 3],
+                [4000, 4],
+                [5000, 5],
+                [10000, 8],
+                [20000, 16],
+                [30000, 24],
+                [40000, 50],
+            ]
+        }
+    ]
+    const mapContainerRef = useRef(null);
+    const [active, setActive] = useState(options[0]) // property to display
+    const [map, setMap] = useState(null)
+
+    // Init map when component mounts
+    useEffect(() => {
+        // Init mapbox object
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: "mapbox://styles/mapbox/streets-v11",
+            center: [106, 10],
+            zoom: 5
+        });
+
+        map.on("load", () => {
+            // load your data here
+            console.log(data);
+            map.addSource("districts", {
+                type: "geojson",
+                data,
+                cluster: true,
+            });
+
+            // add mapbox layer
+            map.addLayer({
+                id: "districts",
+                type: "circle",
+                source: "districts"
+            })
+
+            // set Paint property for Layer
+            map.setPaintProperty("districts", "circle-color", {
+                property: active.property,
+                stops: active.colorStops
+            });
+            map.setPaintProperty("districts", "circle-radius", {
+                property: active.property,
+                stops: active.radiusStops
+            });
+
+            setMap(map);
+            debugger;
+        });
+
+        // Clean up on unmount
+        return () => map.remove();
+    }, []);
+
+    useEffect(() => {
+        paint();
+    }, [active])
+
+    const paint = () => {
+        if (map){
+            map.setPaintProperty("districts", "circle-color", {
+                property: active.property,
+                stops: active.stops
+            });
+        }
+    }
+    return (
+        <div>
+            <div ref={mapContainerRef} className="map-container" />
+        </div>
+    );
+};
+
+export default Map;
