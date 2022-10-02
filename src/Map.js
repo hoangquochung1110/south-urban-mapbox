86b04d91e99cbf1dae9f42d9f00b26240171d4ce
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2022-10-02 16:58:01 +0700

    Improve UI
    - Add legend with color table
    - Add options field to switch between stats

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,12 @@
 import mapboxgl from "mapbox-gl";
+import Legend from "./components/Legend";
+import Optionsfield from './components/Optionsfield';
 import React, { useEffect, useState, useRef } from 'react';
 import "./Map.css";
 import data from "../hochiminh_population_geo.json";
 
 mapboxgl.accessToken = "pk.eyJ1IjoiaG9hbmdxdW9jaHVuZzExMTAiLCJhIjoiY2w4Ym55OXRqMDB0bjNvcGRycDN2MGRuZSJ9.EEFhms7cuNTpkRH-TxfVPw"
+
 
 const Map = () => {
     const options = [
@@ -34,6 +37,41 @@ const Map = () => {
                 [30000, 24],
                 [40000, 50],
             ]
+        },
+        {
+            name: "Density",
+            description: "Population per kilometer square",
+            property: "density",
+            colorStops: [
+                [100, "#fafa6e"],
+                [1000, "#aae479"],
+                [2000, "#98de7c"],
+                [3000, "#75d084"],
+                [4000, "#54c18a"],
+                [5000, "#34b28e"],
+                [10000, "#10a18f"],
+                [15000, "#00918d"],
+                [20000, "#008087"],
+                [25000, "#0d707d"],
+                [30000, "#1f5f70"],
+                [35000, "#285061"],
+                [40000, "#2a4858"],
+            ],
+            radiusStops: [
+                [100, 1],
+                [1000, 1],
+                [2000, 2],
+                [3000, 3],
+                [4000, 4],
+                [5000, 5],
+                [10000, 8],
+                [15000, 14],
+                [20000, 16],
+                [25000, 20],
+                [30000, 24],
+                [35000, 30],
+                [40000, 50],
+            ]
         }
     ]
     const mapContainerRef = useRef(null);
@@ -45,7 +83,7 @@ const Map = () => {
         // Init mapbox object
         const map = new mapboxgl.Map({
             container: mapContainerRef.current,
-            style: "mapbox://styles/mapbox/streets-v11",
+            style: "mapbox://styles/mapbox/dark-v10",
             center: [106, 10],
             zoom: 5
         });
@@ -72,7 +110,7 @@ const Map = () => {
                 {"font-scale": 1.2},
                 "\n",
                 {},
-                ["get", "population_in_2019"],
+                ["get", active.property],
                 {
                     "font-scale": 0.9
                 }
@@ -96,6 +134,7 @@ const Map = () => {
                 property: active.property,
                 stops: active.radiusStops
             });
+            map.setPaintProperty("district-label", "text-color", "#FFFFFF")
 
             setMap(map);
         });
@@ -112,13 +151,48 @@ const Map = () => {
         if (map){
             map.setPaintProperty("districts", "circle-color", {
                 property: active.property,
-                stops: active.stops
+                stops: active.colorStops
             });
+            map.setPaintProperty("districts", "circle-radius", {
+                property: active.property,
+                stops: active.radiusStops
+            });
+            map.setLayoutProperty("district-label", "text-field", [
+                "format",
+                ["get", "name"],
+                {"font-scale": 1.2},
+                "\n",
+                {},
+                ["get", active.property],
+                {
+                    "font-scale": 0.9
+                }
+            ])
         }
     }
+
+    const changeState = i => {
+        setActive(options[i]);
+        map.setPaintProperty("districts", "circle-color", {
+            // Update circle-color
+            property: active.property,
+            stops: active.colorStops
+        });
+        map.setPaintProperty("districts", "circle-radius", {
+            property: active.property,
+            stops: active.radiusStops
+        });
+    };
+
     return (
         <div>
             <div ref={mapContainerRef} className="map-container" />
+            <Legend active={active} stops={active.colorStops} />
+            <Optionsfield
+                options={options}
+                property={active.property}
+                changeState={changeState}
+            />
         </div>
     );
 };
